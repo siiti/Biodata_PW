3be97c6ef78f403771a0dc38e6c7cfa1716168c5
siiti <142282128+siiti@users.noreply.github.com>
2023-12-12 09:12:01 +0700

    Add files via upload

---
+++ biodata/main.js
@@ -0,0 +1,7 @@
+let nama = "Nama: Siti Nur Hanifah ";
+let kelas = "Kelas: XI";
+let jurusan = "Jurusan: PPLG";
+let umur = "Umur: 17";
+let alamat = "Alamat: Kp.Padasuka Nagrak";
+
+document.write(nama + "<br>", kelas + "<br>", jurusan + "<br>", umur + "<br>", alamat + "<br>");
